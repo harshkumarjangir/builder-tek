705c72e0cb44d4e33317281865c352aa1de37509
dev-shivam999 <shivamswami982@gmail.com>
2026-03-18 08:17:59 +0530

    nnm

--- src/components/home/HomeHero.jsx
+++ src/components/home/HomeHero.jsx
@@ -73,7 +73,7 @@ const HomeHero = ({ data }) => {
                      <img src={hero.image2}  className="cursor-pointer" alt="" />
                 </motion.div>
       </div>
-      <div className="relative  w-[50%] w-full">
+      <div className="relative  w-[50%] ">
         <img src={hero.sideimage.image1} className="absolute  w-full top-0 right-0" alt="" />
         <img src={hero.sideimage.image2} className="absolute top-1/2  left-[-10%] translate-y-[-50%]" alt="" />
       </div>
